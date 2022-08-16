be03b68d3906fd93b21d5e40384202c1afafc4eb
unnatsharma <unnat.sharma2912@gmail.com>
2022-08-15 23:18:40 -0600

    redux toolkit example added

--- redux-practice/src/store/store.js
+++ redux-practice/src/store/store.js
@@ -1,54 +1,15 @@
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
 
-const initiaState = { counter: 0, showCounter: true };
+import counterSlice from "./CounterSlice";
+import authSlice from "./AuthSlice";
 
-const counterSlice = createSlice({
-  name: "counter",
-  initiaState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.amount;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-
-// const counterReducer = (state = initiaState, action) => {
-//   if (action.type === "increment") {
-//     return {
-//       counter: state.counter + 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "decrement") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-
-//   if (action.type === "") {
-//     return {
-//       counter: state.counter - 1,
-//       showCounter: state.showCounter,
-//     };
-//   }
-//   return state;
-// };
 
 
 const store = configureStore({
-  reducer: counterSlice.reducer,
+  reducer: { counter: counterSlice.reducer, auth: authSlice.reducer },
 });
 
-export const counterActions =  counterSlice.actions;
+
+
+
 export default store;
